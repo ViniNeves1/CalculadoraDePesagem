e39abc37aec6a4ee13204b9a5182e668faac3eb9
Vinícius dos Santos <146591100+ViniNeves1@users.noreply.github.com>
2025-09-01 21:53:08 -0300

    Função do Peso Bruto sem Peso Bruto Total

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const pbtInput = document.getElementById("pbt");
 const pbInput = document.getElementById("pb");
+const plInput = document.getElementById("pl");
 const tCxInput = document.getElementById("tCx");
+const ttInput = document.getElementById("tt");
 const ppInput = document.getElementById("pp");
 const cxInput = document.getElementById("cx");
 const btn = document.getElementById("btnCalc");
 
+
 function calcularPesos() {
-  //Pegando os VALORES dos inpu ts e convertendo para número
+  //Pegando os VALORES dos inputs e convertendo para número
   const pbt = parseFloat(pbtInput.value) || 0;
   let pb = parseFloat(pbInput.value) || 0;
   const tCx = parseFloat(tCxInput.value) || 0;
@@ -22,25 +25,41 @@ function calcularPesos() {
     //Calcula o Peso Bruto
     pb = pbt - pp;
     //Atualiza o campo no HTML para o usuário ver o resultado
-    pbInput.value = pb.toFixed(3); // .toFixed(3) para formatar com 3 casas decimais
+    pbInput.value = pb.toFixed(3);
+     // .toFixed(3) para formatar com 3 casas decimais
+  } else {
+    pl = pb - tt;
+
+
   }
 
   //Calcula a Tara Total
   tt = tCx * cx;
+  ttInput.value = tt.toFixed(3);
 
   // Calcula o Peso Líquido
   pl = pb - tt;
+  plInput.value = pl.toFixed(3);
+}
 
-  // Exibe os resultados no console
-  console.log("--------------------");
-  console.log("Peso Bruto Total (PBT):", pbt);
-  console.log("Peso do Palete:", pp);
-  console.log("Peso Bruto (PB):", pb);
-  console.log("Tara por Caixa:", tCx);
-  console.log("Quantidade de Caixas:", cx);
-  console.log("Sua Tara Total (TT) é:", tt);
-  console.log("Seu Peso Líquido (PL) é:", pl);
-  console.log("--------------------");
+function rightDate() {
+
+  // 1. Pega o elemento input
+  const dataInput = document.getElementById("val");
+
+  //2. Obtém o valor do input (ex: "2025-09-01")
+  const valorData = dataInput.value;
+
+  //3. Cria um novo objeto Date com base no valor do input
+  const dataObjeto = new Date(valorData);
+
+  //4. Extrai o dia, mês e ano
+  const dia = dataObjeto.getDate(); //retorna o dia do mês (1-31)
+  const mes = dataObjeto.getMonth() +  1; // Retorna o mês (0-11), por isso somamos 1
+  const ano = dataObjeto.getFullYear(); // Retorna o ano com 4 dígitos
+
+  let data = new Date(dataInput);
+  data.setDate(data.getDate() - 10);
 }
 
 btn.addEventListener('click', calcularPesos);
